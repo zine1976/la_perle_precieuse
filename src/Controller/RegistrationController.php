64b4726c916ba7e1e24b6e5bef93c5759c877f0a
zine el abidine <mahdizmen@hotmail.com>
2022-05-05 10:52:36 +0200

    mail

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,27 +3,32 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Security\EmailVerifier;
 use Symfony\Component\Mime\Address;
+use App\Security\SecurityAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
-    // private EmailVerifier $emailVerifier;
+    private EmailVerifier $emailVerifier;
 
-    // public function __construct(EmailVerifier $emailVerifier)
-    // {
-    //     $this->emailVerifier = $emailVerifier;
-    // }
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
 
     /**
      * @Route("/registration", name="app_registration")
      */
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, Authenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, SecurityAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
